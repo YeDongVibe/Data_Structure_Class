0e040d7e8a11d1d28258b044bcd8ba78c11183c1
YeDongVibe <wkwhstladmss@naver.com>
2023-04-26 20:13:43 +0900

    23.04.26

--- Data_Structure/src/Chapter02/ArraySum.java
+++ Data_Structure/src/Chapter02/ArraySum.java
@@ -4,51 +4,59 @@ import java.util.Random;
 
 public class ArraySum {
 
-		static void getArr (int [][] arr) {
-			//난수 생성하여 배열에 입력
-			Random rand = new Random(System.currentTimeMillis()); //초기 ceed값
-			for(int i = 0; i <arr.length; i++) {
-				for(int j = 0; j < arr.length; j++) {
-					arr [i][j] = rand.nextInt(10); //10이하 정수 출력
-				}
-
+	static void getArr(int[][] arr) {
+		// 난수 생성하여 배열에 입력
+		Random rand = new Random(System.currentTimeMillis()); // 초기 ceed값
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				arr[i][j] = rand.nextInt(10); // 10이하 정수 출력
 			}
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
 
 	}
-		static void ShowMatrix (int [][] arr) {
-			for(int i = 0; i<2; i++) {
-				for(int j = 0; j <3; j++) {
-					System.out.print(arr[i][j] + "\t");
-				}
-				System.out.println("\n");
+
+	static void ShowMatrix(int arr[][]) {
+		System.out.println(arr + " 행렬의 데이터");
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(arr[i][j] + "\t");
+			}
+			System.out.println("\n");
+		}
+	}
+
+	static int[][] SumMatrix(int[][] A, int[][] A1) {
+		int[][] A2 = new int[2][3];
+		for (int i = 0; i < 2; i++) {
+
+			for (int j = 0; j < 3; j++) {
+
+				A2[i][j] = A[i][j] + A1[i][j];
 			}
 		}
-		
-		static int[][] SumMatrix(int[][] A, int[][] A1) { 
-			int[][] A2 = new int[2][3];
-			for (int i = 0; i < 2; i++) {
+		return A2;
+	}
 
-				for (int j = 0; j < 3; j++) { 
+	public static void main(String[] args) {
+		int[][] A = new int[2][3];
+		int[][] A1 = new int[2][3];
+		int[][] B = new int[3][4];
+		int[][] C = new int[2][4];
+		int[][] D = new int[3][2];
+		int[][] A2 = SumMatrix(A, A1);
 
-					A2[i][j] = A[i][j] + A1[i][j];
-				}
-			}
-			return A2;
-		}
-		
-		public static void main(String[] args) {
-			int [][] A = new int [2][3];
-			int [][] A1 = new int [2][3];
-			int [][] B = new int [3][4];
-			int [][] C = new int [2][4];
-			int [][] D = new int [3][2];
-			int [][] A2 = SumMatrix(A, A1);
-			
-			getArr(A);
-			ShowMatrix(A);
-			getArr(A1);
-			getArr(A2);
-			ShowMatrix(A2);
+		getArr(A);
+		ShowMatrix(A);
+		getArr(A1);
+		getArr(A2);
+		ShowMatrix(A2);
 
-}
+	}
 }
