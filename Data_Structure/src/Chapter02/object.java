81b5bf7065b0a1631c59eba2f8efed81bfbf3481
YeDongVibe <wkwhstladmss@naver.com>
2023-05-03 09:40:27 +0900

    23.05.03

---
+++ Data_Structure/src/Chapter02/object.java
@@ -0,0 +1,19 @@
+package Chapter02;
+
+public class object {
+	
+	static void sortData(String []items) {
+		for(int i = 0; i < items.length;i++) {
+			for (int j = 0; j < items.length; j++) {
+				if (items[i].compareTo(null) > items[])
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String [] data = {"apple", "grape", "melon"};
+		
+
+	}
+
+}
