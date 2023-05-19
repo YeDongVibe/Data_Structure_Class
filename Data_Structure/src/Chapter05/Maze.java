c79ab53e373f2c7450dfb713809a06279bf4db0e
YeDongVibe <wkwhstladmss@naver.com>
2023-05-19 14:18:00 +0900

    23.05.19 maze

---
+++ Data_Structure/src/Chapter05/Maze.java
@@ -0,0 +1,129 @@
+package Chapter05;
+
+enum Directions {
+	N, NE, E, SE, S, SW, W, NW
+}
+
+class Items {
+	int x;
+	int y;
+	int dir;
+
+}
+
+class Offsets {
+	int a;
+	int b;
+
+}
+
+public class Maze {
+
+	static Offsets[] moves = new Offsets[8];// static을 선언하는 이유를 알아야 한다
+
+	public static void path(int[][] maze, int[][] mark, int ix, int iy) {
+	
+		mark[1][1] = 1;
+		StackList st = new StackList(50);
+		Items temp = new Items(0, 0, 0);//N :: 0
+		temp.x = 1;
+		temp.y = 1;
+		temp.dir = 2;//E:: 2
+		mark[temp.x][temp.y] = 2;//미로 찾기 궤적은 2로 표시
+		st.push(temp);
+
+		while (!st.isEmpty()) // stack not empty
+		{
+			Items tmp = st.pop(); // unstack
+			int i = tmp.x;
+			int j = tmp.y;
+			int d = tmp.dir;
+			mark[i][j] = 1;//backtracking 궤적은 1로 표시
+			while (d < 8) // moves forward
+			{
+
+				if ((g == ix) && (h == iy)) { // reached exit
+												// output path
+
+				}
+				if ((maze[g][h] == 0) && (mark[g][h] == 0)) { // new position
+					
+
+				} else
+				
+			}
+		}
+		System.out.println("no path in maze ");
+	}
+
+	public static void main(String[] args) {
+		int[][] maze = new int[14][17];
+		int[][] mark = new int[14][17];
+
+		int input[][] = { // 12 x 15
+				{ 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1 }, 
+				{ 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1 },
+				{ 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1 }, 
+				{ 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0 },
+				{ 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1 }, 
+				{ 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 },
+				{ 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 }, 
+				{ 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+				{ 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1 }, 
+				{ 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0 },
+				{ 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 }, 
+				{ 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0 } };
+		for (int ia = 0; ia < 8; ia++)
+			moves[ia] = new Offsets(0, 0);// 배열에 offsets 객체를 치환해야 한다.
+		moves[0].a = -1;
+		moves[0].b = 0;
+		moves[1].a = -1;
+		moves[1].b = 1;
+		moves[2].a = 0;
+		moves[2].b = 1;
+		moves[3].a = 1;
+		moves[3].b = 1;
+		moves[4].a = 1;
+		moves[4].b = 0;
+		moves[5].a = 1;
+		moves[5].b = -1;
+		moves[6].a = 0;
+		moves[6].b = -1;
+		moves[7].a = -1;
+		moves[7].b = -1;
+		// Directions d;
+		// d = Directions.N;
+		// d = d + 1;//java는 지원안됨
+		for (int i = 0; i < 14; i++) {
+			for (int j = 0; j < 17; j++) {
+
+			}
+		}
+		System.out.println("maze[12,15]::");
+		for (int i = 0; i <= 13; i++) {
+			for (int j = 0; j <= 16; j++) {
+				System.out.print(maze[i][j] + " ");
+
+			}
+			System.out.println();
+		}
+		System.out.println("mark::");
+		for (int i = 0; i <= 13; i++) {
+			for (int j = 0; j <= 16; j++) {
+				System.out.print(mark[i][j] + " ");
+
+			}
+			System.out.println();
+		}
+		path(maze, mark, 12, 15);
+		System.out.println("mark::");
+		for (int i = 1; i <= 12; i++) {
+			for (int j = 1; j <= 15; j++) {
+				System.out.print(mark[i][j] + " ");
+
+			}
+			System.out.println();
+		}
+
+	}
+}
