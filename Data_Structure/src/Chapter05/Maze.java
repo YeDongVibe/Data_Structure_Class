6f5979d6345ac909674f462679280605b670e402
YeDongVibe <wkwhstladmss@naver.com>
2023-05-22 18:01:24 +0900

    23.05.22 maze

--- Data_Structure/src/Chapter05/Maze.java
+++ Data_Structure/src/Chapter05/Maze.java
@@ -4,14 +4,14 @@ enum Directions {
 	N, NE, E, SE, S, SW, W, NW
 }
 
-class Items {
+class Items {//현재 위치
 	int x;
 	int y;
-	int dir;
+	int dir; //direction
 
 }
 
-class Offsets {
+class Offsets  {
 	int a;
 	int b;
 
@@ -24,7 +24,7 @@ public class Maze {
 	public static void path(int[][] maze, int[][] mark, int ix, int iy) {
 	
 		mark[1][1] = 1;
-		StackList st = new StackList(50);
+		ObjectStack st = new ObjectStack(50);
 		Items temp = new Items(0, 0, 0);//N :: 0
 		temp.x = 1;
 		temp.y = 1;
@@ -75,22 +75,14 @@ public class Maze {
 				{ 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0 } };
 		for (int ia = 0; ia < 8; ia++)
 			moves[ia] = new Offsets(0, 0);// 배열에 offsets 객체를 치환해야 한다.
-		moves[0].a = -1;
-		moves[0].b = 0;
-		moves[1].a = -1;
-		moves[1].b = 1;
-		moves[2].a = 0;
-		moves[2].b = 1;
-		moves[3].a = 1;
-		moves[3].b = 1;
-		moves[4].a = 1;
-		moves[4].b = 0;
-		moves[5].a = 1;
-		moves[5].b = -1;
-		moves[6].a = 0;
-		moves[6].b = -1;
-		moves[7].a = -1;
-		moves[7].b = -1;
+		moves[0].a = -1;	moves[0].b = 0;
+		moves[1].a = -1;	moves[1].b = 1;
+		moves[2].a = 0;		moves[2].b = 1;
+		moves[3].a = 1;		moves[3].b = 1;
+		moves[4].a = 1;		moves[4].b = 0;
+		moves[5].a = 1;		moves[5].b = -1;
+		moves[6].a = 0;		moves[6].b = -1;
+		moves[7].a = -1;	moves[7].b = -1;
 		// Directions d;
 		// d = Directions.N;
 		// d = d + 1;//java는 지원안됨
